ac340aebecb116763002f7ee8e9fabc75d01d72c
manornot <ormanis1609@gmail.com>
2024-10-03 10:58:24 +0300

    mod

--- fileRead.c
+++ fileRead.c
@@ -2,17 +2,19 @@
 #include <string.h>
 int main()
 {
-    char buffer[100];
+    char buffer[500];
     char line[5];
     FILE *fp;
     fp = fopen("test.txt","r");
     while(fgets(line,5,fp)!=NULL)
     {
+        printf("%s\n",line);
         strcat(buffer,line);
     }
     fclose(fp);
     printf("%s",buffer);
 
+return 0;
     
     
 }
