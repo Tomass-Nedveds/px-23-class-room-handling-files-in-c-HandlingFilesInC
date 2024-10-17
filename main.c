33c015006364be6f28c1fbb2536e510b90377cfd
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2024-10-17 07:55:09 +0000

    Commit main.v

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     char buffer[100];
     FILE *fp;
     fp = fopen("test.txt","r");
-    fscanf(fp,"%s",buffer);
+    fscanf(fp,"%s",buffer); //save fp in buffer
     printf("Buffer: %s\n",buffer );
     fclose(fp);
 }
