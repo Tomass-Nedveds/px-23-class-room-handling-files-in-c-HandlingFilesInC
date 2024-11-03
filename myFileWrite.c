b18d66cb97cc61ecb3b7ff2a70f57566ca43498d
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2024-11-03 13:18:45 +0000

    Ex 3 pass

--- myFileWrite.c
+++ myFileWrite.c
@@ -2,14 +2,36 @@
 #include <string.h>
 
 int main(){
-    char filename[50];
-    char text[1000];
+    char text[42];
+    char fileName[20];
+    char name;
 
-    printf("Enter file name: ");
-    scanf("%s", filename);
+    printf("Input file nme: ");
+    scanf("%s", fileName);
+    FILE *fp = fopen(fileName, "r");
+    
+    if(!fp){
+        printf("error\n");
+        return 1;
+    }
 
-    printf("Write text: ");
-    scanf("%s", text);
+    fclose(fp);
+    printf("Enter text: ");
+    getchar();
+    fgets(text, sizeof(text), stdin);
+    printf("w or a:  ");
+    scanf("%c", &name);
 
+    if(name == 'a'){
+        FILE *fp = fopen(fileName, "a");
+        fprintf(fp, "%s", text);
+        fclose(fp);
+    }
 
+    else if(name == 'w'){
+        FILE *fp = fopen(fileName, "w");
+        fprintf(fp, "%s", text);
+        fclose(fp);
+    }
+    return 0;
 }
