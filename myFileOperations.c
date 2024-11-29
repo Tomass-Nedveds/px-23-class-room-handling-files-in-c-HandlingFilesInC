86f85f12ce94d98bc401e7577d8398a3e5559949
Gagrio <95764058+Tomass-Nedveds@users.noreply.github.com>
2024-11-29 19:33:41 +0000

    myFileOperation 80% finished

--- myFileOperations.c
+++ myFileOperations.c
@@ -3,11 +3,13 @@
 
 int main(){
     char fileName[50];
-    char buffer[100];
-    char line[10];
+    char buffer[999];
+    char line[999];
     char text[100];
     int num;
     int sum = 0;
+    int lineCount = 0;
+    int end;
 
     printf("1.Reading file\n2.Writing to file\n3.Appending to file\n4.Counting words, lines, and characters in a file\n");
     printf("------------------\n");
@@ -18,33 +20,37 @@ int main(){
     scanf("%s", fileName);
     getchar();
     FILE *fp = fopen(fileName, "r");
-    while(fgets(line,100,fp)!=NULL)
+
+    while(fgets(line,999,fp)!=NULL)
             strcat(buffer,line);
+    
+    printf("==================\n");
+    printf("%s", buffer);
+    printf("\n==================\n");
+    fclose(fp);
 
     if(num == 1){
-        fclose(fp);
-        printf("==========\n");
         printf("%s \n", buffer);
-
     }
+
     if(num == 2){
-        printf("Enter text: ");
-        fgets(text, sizeof(text), stdin);
-        FILE *fp = fopen(fileName, "a");
-        fprintf(fp, "%s", text);
-        fclose(fp);
-        
-    }
-
-    if(num == 3){
         printf("Enter text: ");
         fgets(text, sizeof(text), stdin);
         FILE *fp = fopen(fileName, "w");
         fprintf(fp, "%s", text);
         fclose(fp);
+    }
+
+    if(num == 3){
+        printf("Enter text: ");
+        fgets(text, sizeof(text), stdin);
+        FILE *fp = fopen(fileName, "a");
+        fprintf(fp, "%s", text);
+        fclose(fp);
         }
     
     if(num == 4){
+        FILE *fp = fopen(fileName, "r");
         int i = 0;
         while(buffer[i] != '\0'){
             if(buffer[i] >= 'A' && buffer[i] <= 'Z' || buffer[i] >= 'a' && buffer[i] <= 'z'){
@@ -54,8 +60,17 @@ int main(){
             }
         i++;
         }
-        printf("%d", sum);
-    }
 
+        while ((end = fgetc(fp)) != EOF) {
+            if (end == '\n') {
+                lineCount++;
+         }   
+    }
+    
+        printf("Sum of line: %d\n", lineCount);
+        printf("Sum of words: %d\n", sum);
+        fclose(fp);
+    }
+    
     return 0;
 }
